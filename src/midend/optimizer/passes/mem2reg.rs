86f955b9b5bf6ffb90d0e0efbfbde6c2cc8f3faa
bu yi <yi.bu@rwth-aachen.de>
2026-07-13 17:37:58 +0800

    add mem2reg

---
+++ src/midend/optimizer/passes/mem2reg.rs
@@ -0,0 +1,26 @@
+use inkwell::module::Module;
+
+use super::Pass;
+
+pub struct Mem2Reg;
+
+impl Pass for Mem2Reg {
+    fn name(&self) -> &'static str {
+        "mem2reg"
+    }
+
+    fn description(&self) -> &'static str {
+        "LLVM mem2reg: promotes alloca/load/store to SSA register values"
+    }
+
+    fn run(&self, module: &Module) -> bool {
+        crate::midend::optimizer::init_native_target();
+        match crate::midend::optimizer::run_llvm_optimizations(module, "mem2reg") {
+            Ok(changed) => changed,
+            Err(e) => {
+                eprintln!("[mem2reg] failed: {}", e);
+                false
+            }
+        }
+    }
+}
